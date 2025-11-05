07a27299241a22fc26ea8fd356b5f7a96fae6211
estolagunju <115906632+estolagunju@users.noreply.github.com>
2025-11-05 22:54:37 +0000

    git push .

--- program.cpp
+++ program.cpp
@@ -1,37 +1,37 @@
 #include "bmp.hpp"
 #include <iostream>
 #include <time.h>
-void draw_rectangle(BMP &bmp,int x, int y, int l, int h, int b, color c ){
+void draw_rectangle(BMP &bmp,int x, int y, int l, int h, color c, color b ){
     for(int i=0;i<=l;i++){
-        for(int ii=0;ii<=b;ii++){
+        for(int ii=0;ii<=h;ii++){
         bmp.set_pixel(i+x, y+ii, c.r,c.g,c.b);
         bmp.set_pixel(i+x, y+h-ii, c.r, c.g,c.b);
-        }
+        } // go through the shape, length and height
     }
-    for(int j=0;j<=h;j++){
+    /*for(int j=0;j<=h;j++){
         for(int ii=0;ii<=b;ii++){
             bmp.set_pixel(x+ii, y+j, c.r,c.g,c.b);
             bmp.set_pixel(x+l-ii, y+j,c.r, c.g,c.b);
-        }
-    }
+        } // set the border of shape
+    }*/
 }
 
 int main() {
     
     BMP bmp(500, 500); 
      srand(static_cast<unsigned int>(time(0)));
-    color c(0,255,0);
+    color c(0,255,0);color b(23,19,60);
     for(int i=0;i<5;i++){
         uint8_t x=rand()%500;
         uint8_t y=rand()%500;
         uint8_t l=rand()%500;
         uint8_t h=rand()%500;
         
-        draw_rectangle(bmp,x,y,l,h,rand()%10,c);
+        draw_rectangle(bmp,x,y,l,h,c,b);//,rand()%10
 
     }
     
-     draw_rectangle(bmp,1,1,500,500,10,color{255,0,0});
+     draw_rectangle(bmp,1,1,500,500,color{255,0,0}, color{23,19,60}); //500,10,
 
     bmp.write("rect.bmp"); // Save the image to a file
 
